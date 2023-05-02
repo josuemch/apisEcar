c0ce415b35b7277613e2c5597e489e7cd9685179
josuemch <jozuemch@gmail.com>
2023-05-02 18:31:33 -0500

    version 1.3

--- app.js
+++ app.js
@@ -2,21 +2,20 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send({
-    "Output": "Hello Mundo get!"
-  });
+    res.send({
+        "Output": "Hello Mundo get version 1.3!"
+    });
 });
 
 app.post('/', function(req, res) {
-  res.send({
-    "Output": "Hello Mundo post!"
-  });
+    res.send({
+        "Output": "Hello Mundo post version 1.3!"
+    });
 });
 
 app.get('/hola-mundo', function(req, res) {
-  res.send('Hola mundo 123');
+    res.send('Hola mundo 123');
 });
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app;
-
