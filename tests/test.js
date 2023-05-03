90f37f6d246210b2ca0042dfdcfb591ae489d412
josuemch <jozuemch@gmail.com>
2023-05-02 19:47:25 -0500

    version 1.4.8

--- tests/test.js
+++ tests/test.js
@@ -9,15 +9,15 @@ const request = supertest(app);
 describe('Tests app', function() {
     it('verifies get', function(done) {
         request.get('/').expect(200).end(function(err, result) {
-            test.string(result.body.Output).contains('Hello');
-            test.value(result.headers['content-type']).is('application/json; charset=utf-8');
+            test.object(result.body).hasProperty('Output').hasValue('Hello Mundo get version 1.3!');
+            test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
             done(err);
         });
     });
     it('verifies post', function(done) {
         request.post('/').expect(200).end(function(err, result) {
-            test.string(result.body.Output).contains('Hello');
-            test.value(result.headers['content-type']).is('application/json; charset=utf-8');
+            test.object(result.body).hasProperty('Output').hasValue('Hello Mundo post version 1.3!');
+            test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
             done(err);
         });
     });
